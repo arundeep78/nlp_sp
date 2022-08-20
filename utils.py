27ac1a81386aab52680b8c7161e4c038f5918a3d
arundeep78 <arundeep.singh@outlook.com>
2022-08-20 18:12:13 +0200

    add LSTM with attention notebook

---
+++ utils.py
@@ -0,0 +1,226 @@
+import re
+import string
+
+import numpy as np
+
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.tokenize import TweetTokenizer
+
+from matplotlib.patches import Ellipse
+import matplotlib.transforms as transforms
+
+
+def clean_tweet(tweet):
+  ''' 
+  Input:
+      tweet: a string containing a tweet
+  Output:
+      tweets_clean: tweet string cleaned of special texts, hyperlinks, strings, characters
+  '''
+
+    # remove stock market tickers like $GE
+    tweet = re.sub(r'\$\w*', '', tweet)
+    # remove old style retweet text "RT"
+    tweet = re.sub(r'^RT[\s]+', '', tweet)
+    # remove hyperlinks
+    #tweet = re.sub(r'https?:\/\/.*[\r\n]*', '', tweet)
+    tweet = re.sub(r'https?://[^\s\n\r]+', '', tweet)
+    # remove hashtags
+    # only removing the hash # sign from the word
+    tweet = re.sub(r'#', '', tweet)
+  
+    return tweet
+
+def process_tweet(tweet):
+    '''
+    Input:
+        tweet: a string containing a tweet
+    Output:
+        tweets_clean: a list of words containing the processed tweet
+
+    '''
+    stemmer = PorterStemmer()
+    stopwords_english = stopwords.words('english')
+  
+    # clean tweet of special characters, hyperlinks etc.
+    tweet = clean_tweet(tweet)
+
+    # tokenize tweets
+    tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True,
+                               reduce_len=True)
+    tweet_tokens = tokenizer.tokenize(tweet)
+
+    tweets_clean = []
+    for word in tweet_tokens:
+        if (word not in stopwords_english and  # remove stopwords
+                word not in string.punctuation):  # remove punctuation
+            # tweets_clean.append(word)
+            stem_word = stemmer.stem(word)  # stemming word
+            tweets_clean.append(stem_word)
+
+    return tweets_clean
+
+
+
+def create_word_sentiment_dict( tweets, ys):
+    '''
+    Input:
+        tweets: a list of tweets. each tweet is a string
+        ys: a list corresponding to the sentiment of each tweet (either 0(neg) or 1(pos))
+    Output:
+        result: a dictionary mapping each pair (word, sentiment) to its frequency
+    '''
+
+    for y, tweet in zip(ys, tweets):
+        for word in process_tweet(tweet):
+            # define the key, which is the word and label tuple
+            pair = (word,y)
+            
+            result[pair] = result.get(pair,0) + 1
+
+    return result
+
+def test_lookup(func):
+    freqs = {('sad', 0): 4,
+             ('happy', 1): 12,
+             ('oppressed', 0): 7}
+    word = 'happy'
+    label = 1
+    if func(freqs, word, label) == 12:
+        return 'SUCCESS!!'
+    return 'Failed Sanity Check!'
+
+
+def lookup(freqs, word, label):
+    '''
+    Input:
+        freqs: a dictionary with the frequency of each pair (or tuple)
+        word: the word to look up
+        label: the label corresponding to the word
+    Output:
+        n: the number of times the word with its corresponding label appears.
+    '''
+    n = 0  # freqs.get((word, label), 0)
+
+    pair = (word, label)
+    if (pair in freqs):
+        n = freqs[pair]
+
+    return n
+
+# From: https://matplotlib.org/3.1.1/gallery/statistics/confidence_ellipse.html#sphx-glr-gallery-statistics-confidence-ellipse-py
+
+
+def confidence_ellipse(x, y, ax, n_std=3.0, facecolor='none', **kwargs):
+    """
+    Create a plot of the covariance confidence ellipse of `x` and `y`
+
+    Parameters
+    ----------
+    x, y : array_like, shape (n, )
+        Input data.
+
+    ax : matplotlib.axes.Axes
+        The axes object to draw the ellipse into.
+
+    n_std : float
+        The number of standard deviations to determine the ellipse's radiuses.
+
+    Returns
+    -------
+    matplotlib.patches.Ellipse
+
+    Other parameters
+    ----------------
+    kwargs : `~matplotlib.patches.Patch` properties
+    """
+    if x.size != y.size:
+        raise ValueError("x and y must be the same size")
+
+    cov = np.cov(x, y)
+    pearson = cov[0, 1] / np.sqrt(cov[0, 0] * cov[1, 1])
+    # Using a special case to obtain the eigenvalues of this
+    # two-dimensionl dataset.
+    ell_radius_x = np.sqrt(1 + pearson)
+    ell_radius_y = np.sqrt(1 - pearson)
+    ellipse = Ellipse((0, 0),
+                      width=ell_radius_x * 2,
+                      height=ell_radius_y * 2,
+                      facecolor=facecolor,
+                      **kwargs)
+
+    # Calculating the stdandard deviation of x from
+    # the squareroot of the variance and multiplying
+    # with the given number of standard deviations.
+    scale_x = np.sqrt(cov[0, 0]) * n_std
+    mean_x = np.mean(x)
+
+    # calculating the stdandard deviation of y ...
+    scale_y = np.sqrt(cov[1, 1]) * n_std
+    mean_y = np.mean(y)
+
+    transf = transforms.Affine2D() \
+        .rotate_deg(45) \
+        .scale(scale_x, scale_y) \
+        .translate(mean_x, mean_y)
+
+    ellipse.set_transform(transf + ax.transData)
+    return ax.add_patch(ellipse)
+
+
+
+def assign_unk(tok):
+    """
+    Assign unknown word tokens
+    """
+    # Digits
+    if any(char.isdigit() for char in tok):
+        return "--unk_digit--"
+
+    # Punctuation
+    elif any(char in punct for char in tok):
+        return "--unk_punct--"
+
+    # Upper-case
+    elif any(char.isupper() for char in tok):
+        return "--unk_upper--"
+
+    # Nouns
+    elif any(tok.endswith(suffix) for suffix in noun_suffix):
+        return "--unk_noun--"
+
+    # Verbs
+    elif any(tok.endswith(suffix) for suffix in verb_suffix):
+        return "--unk_verb--"
+
+    # Adjectives
+    elif any(tok.endswith(suffix) for suffix in adj_suffix):
+        return "--unk_adj--"
+
+    # Adverbs
+    elif any(tok.endswith(suffix) for suffix in adv_suffix):
+        return "--unk_adv--"
+
+    return "--unk--"
+
+def get_word_tag(line, vocab): 
+    if not line.split():
+        word = "--n--"
+        tag = "--s--"
+        return word, tag
+    else:
+        word, tag = line.split()
+        if word not in vocab: 
+            # Handle unknown words
+            word = assign_unk(word)
+        return word, tag
+    return None 
+
+
+
+def sigmoid(z):
+    # sigmoid function
+    return 1.0 / (1.0 + np.exp(-z))
+
+
